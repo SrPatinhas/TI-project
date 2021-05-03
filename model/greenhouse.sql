a5bd0feb24485cab84705f0dbbd883f9b0696dea
SrPatinhas <migas_cerejo-1993@hotmail.com>
2021-05-03 18:57:56 +0100

    add create db

--- model/greenhouse.sql
+++ model/greenhouse.sql
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `greenhouse`
 --
+CREATE DATABASE IF NOT EXISTS `greenhouse` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `greenhouse`;
 
 -- --------------------------------------------------------
 
